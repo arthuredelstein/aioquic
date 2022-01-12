a469c773138a6bf5fd440923e89554bb97f0eeb2
Arthur Edelstein <arthuredelstein@gmail.com>
2022-01-12 20:56:27 +0000

    always use the latest version of certificate

--- run_privacytests_org.sh
+++ run_privacytests_org.sh
@@ -1,1 +1,1 @@
-python3 examples/http3_server.py --certificate /home/arthur/certs/h3.arthuredelstein.net/cert1.pem --private-key /home/arthur/certs/h3.arthuredelstein.net/privkey1.pem  -v
+python3 examples/http3_server.py --certificate /etc/letsencrypt/live/h3.arthuredelstein.net/fullchain.pem --private-key /etc/letsencrypt/live/h3.arthuredelstein.net/privkey.pem  -v
