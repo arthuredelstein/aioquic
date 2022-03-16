d5b540409ca974788d9df3d0d04ac6bb7c77d21d
Arthur Edelstein <arthuredelstein@gmail.com>
2022-03-16 22:46:57 +0000

    use single certificate for h3.arthuredelstein.net and altsvc.arthuredelstein.net

--- run_privacytests_org.sh
+++ run_privacytests_org.sh
@@ -1,1 +1,1 @@
-python3 examples/http3_server.py --certificate /etc/letsencrypt/live/h3.arthuredelstein.net/fullchain.pem --private-key /etc/letsencrypt/live/h3.arthuredelstein.net/privkey.pem  -v
+python3 examples/http3_server.py --certificate /etc/letsencrypt/live/altsvc.arthuredelstein.net/fullchain.pem --private-key /etc/letsencrypt/live/altsvc.arthuredelstein.net/privkey.pem  -v
