0df4f74e8b8201a8a8fdd337c36c6becbd02e323
Arthur Edelstein <arthuredelstein@gmail.com>
2022-06-18 19:33:22 +0000

    run two aioquic servers, one for altsvc and one for h3

--- run_privacytests_org.sh
+++ run_privacytests_org.sh
@@ -1,1 +1,2 @@
-python3 examples/http3_server.py --certificate /etc/letsencrypt/live/altsvc.arthuredelstein.net/fullchain.pem --private-key /etc/letsencrypt/live/altsvc.arthuredelstein.net/privkey.pem  -v
+python3 examples/http3_server.py --port 4433 --certificate /etc/letsencrypt/live/altsvc.arthuredelstein.net/fullchain.pem --private-key /etc/letsencrypt/live/altsvc.arthuredelstein.net/privkey.pem  -v &
+python3 examples/http3_server.py --port 4434 --certificate /etc/letsencrypt/live/h3.arthuredelstein.net/fullchain.pem --private-key /etc/letsencrypt/live/h3.arthuredelstein.net/privkey.pem  -v &
