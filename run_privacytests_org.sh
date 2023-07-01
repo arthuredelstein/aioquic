f9e43ffdce270a4937ce0b941911509aa1637217
Arthur Edelstein <arthuredelstein@gmail.com>
2023-07-01 01:07:41 +0000

    altsvc.privacytests3.org

--- run_privacytests_org.sh
+++ run_privacytests_org.sh
@@ -1,2 +1,5 @@
-python3 examples/http3_server.py --port 4433 --certificate /etc/letsencrypt/live/altsvc.arthuredelstein.net/fullchain.pem --private-key /etc/letsencrypt/live/altsvc.arthuredelstein.net/privkey.pem  -v &
-python3 examples/http3_server.py --port 4434 --certificate /etc/letsencrypt/live/h3.arthuredelstein.net/fullchain.pem --private-key /etc/letsencrypt/live/h3.arthuredelstein.net/privkey.pem  -v &
+python3 examples/http3_server.py --port 4433 --certificate /etc/letsencrypt/live/altsvc.privacytests2.org/fullchain.pem --private-key /etc/letsencrypt/live/altsvc.privacytests2.org/privkey.pem  -v &
+
+python3 examples/http3_server.py --port 4434 --certificate /etc/letsencrypt/live/h3.privacytests2.org/fullchain.pem --private-key /etc/letsencrypt/live/h3.privacytests2.org/privkey.pem  -v &
+
+python3 examples/http3_server.py --port 4435 --certificate /etc/letsencrypt/live/altsvc.privacytests3.org/fullchain.pem --private-key /etc/letsencrypt/live/altsvc.privacytests3.org/privkey.pem  -v &
